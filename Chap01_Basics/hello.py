bea803a02b150b0f37173e7bcaf1423a45705774
Ganesh Na <gnanda1987@gmail.com>
2020-05-28 04:31:06 -0700

    Basics of Python

---
+++ Chap01_Basics/hello.py
@@ -0,0 +1,5 @@
+#!/usr/bin/env python3
+
+# Simple Hello program in Python 3.
+
+print('Hello, World.')
---
+++ Chap01_Basics/python-version.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python3
+# Simple program to print python version 
+
+import platform
+
+print('Python Version: ', platform.python_version())
